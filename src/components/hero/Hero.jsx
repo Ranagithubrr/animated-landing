9fda71d9521d2cbc2e3fa47811be0ca88668c5ac
Your Name <ranarr.dev@gmail.com>
2024-12-05 16:30:02 +0600

    Feat: donw arrow animation added with motion

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,16 +1,41 @@
-import { SlArrowDownCircle } from 'react-icons/sl';
-import HeroBg from '../../assets/img/hero-bg.webp';
+import { SlArrowDownCircle } from "react-icons/sl";
+import HeroBg from "../../assets/img/hero-bg.webp";
+import { motion } from "motion/react";
 const Hero = () => {
   return (
-    <div className="h-screen max-w-screen-2xl mx-auto flex items-left justify-center flex-col -mt-36"
-    style={{ backgroundImage: `url(${HeroBg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+    <div
+      className="h-screen max-w-screen-2xl mx-auto flex items-left justify-center flex-col -mt-36"
+      style={{
+        backgroundImage: `url(${HeroBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
     >
-      <h1 className="text-brand-primary text-6xl font-extrabold">EMPOWER YOUR GLOBAL TRADE</h1>
-      <p className="text-brand-primary text-3xl mt-5">with high-efficiency and FTZ solutions.</p>
+      <h1 className="text-brand-primary text-6xl font-extrabold">
+        EMPOWER YOUR GLOBAL TRADE
+      </h1>
+      <p className="text-brand-primary text-3xl mt-5">
+        with high-efficiency and FTZ solutions.
+      </p>
       <div className="absolute right-[10%] bottom-[10%] flex items-center flex-col">
-        <p className='text-center text-brand-primary text-3xl font-semibold'>Scroll more
-         <br />& explore!</p>
-        <span className='text-brand-primary text-5xl mt-5'><SlArrowDownCircle /></span>
+        <p className="text-center text-brand-primary text-3xl font-semibold">
+          Scroll more
+          <br />& explore!
+        </p>
+        <motion.span
+          className="text-brand-primary text-5xl mt-5"
+          animate={{
+            y: [0, 15, 0],
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "loop",
+            ease: "easeInOut",
+          }}
+        >
+          <SlArrowDownCircle />
+        </motion.span>
       </div>
     </div>
   );
