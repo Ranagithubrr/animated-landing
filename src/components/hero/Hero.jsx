ab5cc857368b2493adbdf26859a4e76b3d031d6e
Your Name <ranarr.dev@gmail.com>
2024-12-05 16:24:14 +0600

    Feat: scroll more feature added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,3 +1,4 @@
+import { SlArrowDownCircle } from 'react-icons/sl';
 import HeroBg from '../../assets/img/hero-bg.webp';
 const Hero = () => {
   return (
@@ -6,6 +7,11 @@ const Hero = () => {
     >
       <h1 className="text-brand-primary text-6xl font-extrabold">EMPOWER YOUR GLOBAL TRADE</h1>
       <p className="text-brand-primary text-3xl mt-5">with high-efficiency and FTZ solutions.</p>
+      <div className="absolute right-[10%] bottom-[10%] flex items-center flex-col">
+        <p className='text-center text-brand-primary text-3xl font-semibold'>Scroll more
+         <br />& explore!</p>
+        <span className='text-brand-primary text-5xl mt-5'><SlArrowDownCircle /></span>
+      </div>
     </div>
   );
 };
