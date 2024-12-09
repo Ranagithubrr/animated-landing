bcfbaf8f268e7b55dc4d8e75c17d040825b1273b
Your Name <ranarr.dev@gmail.com>
2024-12-09 14:03:36 +0600

    Fix: keep in touch desktop screen resonsive issue fixed

--- src/components/keepinTouch/KeepInTouch.jsx
+++ src/components/keepinTouch/KeepInTouch.jsx
@@ -11,7 +11,7 @@ const KeepInTouch = () => {
   return (
     <div className="bg-brand-primary">
       <div className="items-center justify-between py-20 mx-auto lg:flex max-w-screen-2xl">
-        <div className="flex flex-col items-center w-full mb-6 text-center lg:mb-0 lg:w-1/2">
+        <div className="flex flex-col items-center w-full mb-6 text-center lg:items-start lg:mb-0 lg:w-1/2">
           <img src={LogoWhite} alt="tri link" className="w-64"/>
           <p className="mt-2 text-3xl font-normal text-gray-100">Keep in touch!</p>
         </div>
